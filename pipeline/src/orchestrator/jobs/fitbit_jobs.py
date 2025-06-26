a65d205ced68b945015f5ff04dd16b4e56db6012
aebel-shajan <aebelshajan@gmail.com>
2025-06-26 13:28:49 +0100

    Add orchestration assets for fitbit exercise transformation

--- pipeline/src/orchestrator/jobs/fitbit_jobs.py
+++ pipeline/src/orchestrator/jobs/fitbit_jobs.py
@@ -6,7 +6,7 @@ fitbit_calories_job = dg.define_asset_job(
     selection=[
         fitbit_assets.fitbit_zip,
         fitbit_assets.fitbit_calories_jsons,
-        fitbit_assets.raw_fitbit_calories,
+        fitbit_assets.fitbit_calories_raw,
         fitbit_assets.fitbit_calories,
     ],
 )

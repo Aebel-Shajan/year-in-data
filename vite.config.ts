1dd46370a83afcfface5790743b4dbb47d127d1a
aebel-shajan <aebelshajan@gmail.com>
2026-01-02 21:10:05 +0000

    Install tailwind setup example layout.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "./", // in built index.html use paths starting at . instead of /
   build: {
     outDir: 'dist-react' // change from default build dir of dist

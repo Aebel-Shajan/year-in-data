6a477128ddcde11c5d7498a437a26f11894c472f
aebel-shajan <aebelshajan@gmail.com>
2026-01-02 22:57:59 +0000

    Experiment with creating db for electron.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,6 +1,8 @@
-import {app, BrowserWindow} from "electron"
+import {app, BrowserWindow, ipcMain} from "electron"
 import path from "path"
 import { isDev } from "./util.js"
+import { etl_screentime, ScreenTimeRecord } from "./etl/screentime.js"
+import { db } from "./electron-db.js"
 
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({})
@@ -11,3 +13,19 @@ app.on("ready", () => {
     mainWindow.loadFile(bundledAppPath)
   }
 })
+
+ipcMain.handle("etl-screen-time", (_event) => {
+  etl_screentime(db)
+  return { success: true};
+});
+
+// IPC handler to fetch records for a specific year
+ipcMain.handle("get-screen-time-by-year", (_event, year: number) => {
+  const stmt = db.prepare(`
+    SELECT * FROM screen_time
+    WHERE strftime('%Y', start_time) = ?
+    ORDER BY start_time DESC
+  `);
+  const records: ScreenTimeRecord[] = stmt.all(year.toString()) as ScreenTimeRecord[];
+  return records;
+});

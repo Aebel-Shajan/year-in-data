1399fa60353d8d86f3ce74d15a4618bc3448b754
aebel-shajan <aebelshajan@gmail.com>
2026-01-02 22:58:18 +0000

    Add code to fetch from electron main process in frontend.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,37 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from './components/ui/button'
 import DarkModeToggle from './components/dark-mode-toggle'
 import { HeatmapVisual } from './components/heatmap-visual'
+import { ipcRenderer } from "electron";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  async function fetchScreenTimeByYear(year: number) {
+    const records = await ipcRenderer.invoke("get-screen-time-by-year", year);
+    console.log(`Screen time for ${year}:`, records);
+    return records;
+  }
+
+  function extractScreenTime() {
+    ipcRenderer.invoke("etl-screen-time");
+    fetchScreenTimeByYear(2025).then(newData => setData(newData))
+  }
+
+  useEffect(() => {
+    fetchScreenTimeByYear(2025).then(newData => setData(newData))
+  }, [])
+
+
+
+
 
   return (
     <>
       <div className='w-full h-full bg-slate-500 flex p-3 gap-2'>
         <div className='w-50 h-full flex flex-col gap-3 bg-background rounded-xl p-3'>
           <DarkModeToggle />
-          <Button variant="outline">
-            Screen time
+          <Button variant="outline" onClick={() => extractScreenTime}>
+            extract screen time
           </Button>
-          <Button variant="outline">
-            Kindle reading
-          </Button>
-
         </div>
         <div className='min-h-full flex-1 bg-background rounded-xl overflow-x-hidden p-3'>
-
           <div className='p-2 outline rounded-xl overflow-scroll'>
-            <HeatmapVisual data={[{
-              "app": "com.google.Chrome",
-              "device_id": "Unknown",
-              "device_model": "Unknown",
-              "usage": 15,
-              "timezone": 0,
-              "created_at": "2026-11-02T15:03:27.295523",
-              "start_time": "2026-11-02T15:03:12",
-              "end_time": "2026-11-02T15:03:27"
-            },]} />
+            <HeatmapVisual data={data} />
           </div>
         </div>
       </div>

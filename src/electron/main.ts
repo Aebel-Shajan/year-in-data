161fff7f9679f4e37fa4a7cf866824bd0dead6d1
aebel-shajan <aebelshajan@gmail.com>
2026-01-03 15:41:48 +0000

    Create preload script to expose electronApi context bridge to frontend.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,31 +1,44 @@
-import {app, BrowserWindow, ipcMain} from "electron"
+import { app, BrowserWindow, ipcMain } from "electron"
 import path from "path"
-import { isDev } from "./util.js"
+import { getPreloadPath, isDev } from "./util.js"
 import { etl_screentime, ScreenTimeRecord } from "./etl/screentime.js"
 import { db } from "./electron-db.js"
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({})
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: getPreloadPath()
+    }
+  })
   if (isDev()) {
     mainWindow.loadURL("http://localhost:5123")
   } else {
     const bundledAppPath = path.join(app.getAppPath(), 'dist-react/index.html')
     mainWindow.loadFile(bundledAppPath)
   }
-})
 
-ipcMain.handle("etl-screen-time", (_event) => {
-  etl_screentime(db)
-  return { success: true};
-});
 
-// IPC handler to fetch records for a specific year
-ipcMain.handle("get-screen-time-by-year", (_event, year: number) => {
-  const stmt = db.prepare(`
+  ipcMain.handle("etl-screen-time", (_event) => {
+    console.log("extracting screen time")
+    try {
+      etl_screentime(db)
+    } catch (exc) {
+      console.log("Error whilst extracting screen time", exc)
+    }
+    return { success: true };
+  });
+
+  // IPC handler to fetch records for a specific year
+  ipcMain.handle("get-screen-time-by-year", (_event, year: number) => {
+    console.log("Getting screen time from db")
+    const stmt = db.prepare(`
     SELECT * FROM screen_time
     WHERE strftime('%Y', start_time) = ?
     ORDER BY start_time DESC
   `);
-  const records: ScreenTimeRecord[] = stmt.all(year.toString()) as ScreenTimeRecord[];
-  return records;
-});
+    const records: ScreenTimeRecord[] = stmt.all(year.toString()) as ScreenTimeRecord[];
+    return records;
+  });
+})
+
+
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,20 +2,19 @@ import { useEffect, useState } from 'react'
 import { Button } from './components/ui/button'
 import DarkModeToggle from './components/dark-mode-toggle'
 import { HeatmapVisual } from './components/heatmap-visual'
-import { ipcRenderer } from "electron";
 
 
 function App() {
   const [data, setData] = useState([])
 
   async function fetchScreenTimeByYear(year: number) {
-    const records = await ipcRenderer.invoke("get-screen-time-by-year", year);
-    console.log(`Screen time for ${year}:`, records);
+    const records = await window.electronAPI.getScreenTimeByYear(year)
+    // console.log(`Screen time for ${year}:`, records);
     return records;
   }
 
-  function extractScreenTime() {
-    ipcRenderer.invoke("etl-screen-time");
+  async function extractScreenTime() {
+    await window.electronAPI.extractScreenTime()
     fetchScreenTimeByYear(2025).then(newData => setData(newData))
   }
 
@@ -32,7 +31,7 @@ function App() {
       <div className='w-full h-full bg-slate-500 flex p-3 gap-2'>
         <div className='w-50 h-full flex flex-col gap-3 bg-background rounded-xl p-3'>
           <DarkModeToggle />
-          <Button variant="outline" onClick={() => extractScreenTime}>
+          <Button variant="outline" onClick={() => extractScreenTime()}>
             extract screen time
           </Button>
         </div>

336881f5bce2c2ac6c815dce5a8ab9460cb3c07a
aebel-shajan <aebelshajan@gmail.com>
2026-01-03 19:00:00 +0000

    Move heatmap-visual.tsx to visualisations.

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Button } from './components/ui/button'
 import DarkModeToggle from './components/dark-mode-toggle'
-import { HeatmapVisual } from './components/heatmap-visual'
+import { HeatmapVisual } from './components/visualisations/heatmap-visual'
 
 
 function App() {

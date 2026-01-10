1b1adaa40bba7cf1aae08336a5b5db3c1d176a9f
aebel-shajan <aebelshajan@gmail.com>
2026-01-10 18:21:46 +0000

    Finish etl script for chatgpt messages.

--- src/electron/electron-db.ts
+++ src/electron/electron-db.ts
@@ -4,13 +4,14 @@ import path from "path";
 import Database from "better-sqlite3";
 import { screenTimeSql } from "./etl/screentime.js";
 import { isDev } from "./util.js";
+import { chatGptMessagesSql } from "./etl/chatGptMessages.js";
 
 const dbPath = isDev()  ? "./year-in-data.db" : path.join(app.getPath("userData"), "year-in-data.db");
 const db = new Database(dbPath);
 
-// Unique constraint prevents inserting the same app record for same device + start time
 const tables = [
-  screenTimeSql
+  screenTimeSql,
+  chatGptMessagesSql
 ]
 tables.forEach(tableSql => {
   db.prepare(tableSql).run()

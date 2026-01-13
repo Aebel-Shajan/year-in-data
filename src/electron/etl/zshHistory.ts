dd2dc0496008e5f8f073fe1df6f4a67709a52a11
aebel-shajan <aebelshajan@gmail.com>
2026-01-13 22:57:23 +0000

    Creaete notebook to investigate zsh history

---
+++ src/electron/etl/zshHistory.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import os from "os";
+
+
+
+
+function etlZshHistory() {
+
+}
+
+
+function extract() {
+  const zshHistoryPath = path.join(
+    os.homedir(),
+    "Library/Application Support/Knowledge/knowledgeC.db"
+  );
+  
+}
